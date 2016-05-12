53d26dd711661db6667e7700423de3bda7757d3c
chenwenbin03 <chenwenbin03@meituan.com>
2016-05-12 15:12:46 +0800

    fix android log with window.onerror

--- log.js
+++ log.js
@@ -1,52 +1,105 @@
 (function(console, debug, commonColor){
+    if(!debug){return;}
+
     if(!console){
         console = window.console = {};
         console.log = function(){};
     }
-    window.addEventListener('error', function(e, filename, lineNo){
-        console.log(e, e.message, 'filename:' + (filename || e.filename), 'lineNo:' + lineNo || (e.lineno), 'colNo:' + e.colno, e.error);
-    });
-    if(!debug){return;}
+    var hasInit = false;
     var clog = console.log;
-    var debugLog = function(){
-        Function.prototype.apply.call(clog, console, arguments);
-    }
+
+    var panel, list;
     var tempConsoleStorage = [];
+
+    var debugLog = function(){
+            Function.prototype.apply.call(clog, console, arguments);
+        },
+        logError = function(e, filename, lineNo, colno, errObj, caller){
+            log('#f00', [
+                e.message || e.error || (e.toString ? e.toString() : e)
+                ]);
+            log('#f00', [
+                '[error]',
+                (filename || e.filename), 
+                (lineNo || e.lineno || '-') + ':' + (colno || e.colno || '-')
+                ]);
+        }
+
+    var errFunc = function(e, filename, lineNo, colno, errObj){
+        if(!hasInit){
+            alert(e.message || e + 'from:' + arguments.callee.caller.toString());
+        }
+        logError(e, filename, lineNo, colno, errObj, arguments.callee.caller);
+    }
+    window.onerror ? window.addEventListener('error', errFunc) : (window.onerror = errFunc);
+
     console.log = function(){
         tempConsoleStorage.push(arguments);
-        Function.prototype.apply.call(clog, console, arguments);
+        debugLog.apply(null, arguments);
     }
     var bindFunc = function(){
-        var panel = document.createElement('div');
-        panel.style.cssText = "position:fixed;top:0;right:0;max-height:100%;overflow:hidden;z-index:999999;opacity:.4;pointer-events:none;";
-        panel.innerHTML = [
-            '<ul style="margin:0;padding:0;">',
-                '<li></li>',
-            '</ul>'
-        ].join('');
-        document.body.appendChild(panel);
+        initPanel();
 
-        var list = panel.children[panel.children.length-1];
-        var log = function(color, args){
-            var msg = [].map.call(args, function(arg){
-                return (arg && arg.toString) ? arg.toString() : arg;
-            }).join(' | ');
-            var li = document.createElement('li');
-            li.style.cssText = 'text-align:right;display:block;padding:5px 12px;border-bottom:1px solid #ccc;background:rgba(255,255,255,.4);color:'+color+';font-size:12px;line-height:1.2;clear:both;word-break:break-all;';
-            li.textContent = msg;
-            list.insertBefore(li, list.firstChild);
-        }
         tempConsoleStorage.forEach(function(args){
             log(commonColor, args);
         });
 
         console.log = function(){
             log(commonColor, arguments);
-            Function.prototype.apply.call(clog, console, arguments);
+            debugLog.apply(null, arguments);
         }
+        hasInit = true;
     };
-    (document.readystate === 'interactive' || document.readystate === 'complete') ?
+    (document.readyState === 'interactive' || document.readyState === 'complete') ?
         bindFunc() :
         document.addEventListener('DOMContentLoaded', bindFunc);
+
+
+    function log(color, args){
+        if(!panel){return;}
+        var msg = [].map.call(args, function(arg){
+            return (arg && arg.toString) ? arg.toString() : arg;
+        }).join(' | ');
+        var li = document.createElement('li');
+        li.style.cssText = [
+            'text-align:right',
+            'display:block',
+            'padding:5px 0px 5px 12px',
+            'border-bottom:1px solid #ccc',
+            'background:rgba(255,255,255,.4)',
+            'color:'+color,
+            'font-size:12px',
+            'font-size:10px',
+            'line-height:1.2',
+            'clear:both',
+            'word-break:break-all'
+            ].join(';');
+        li.textContent = msg;
+        li.innerHTML = '<span>' + li.innerHTML + '</span>' + ' \t<span style="color:#aaa;pointer-events:auto;white-space:nowrap;">' + new Date().toLocaleTimeString('ca', {hour12:false}) + '</span>';
+        list.insertBefore(li, list.firstChild);
+    }
+    function initPanel(){
+        if(!document.body){return;}
+        panel = document.createElement('div');
+        panel.style.cssText = [
+            'position:fixed',
+            'top:0',
+            'right:0',
+            'bottom:0',
+            'max-height:100%',
+            'overflow-y:auto',
+            'z-index:999999',
+            'opacity:.4',
+            'pointer-events:none'
+        ].join(';');
+
+        panel.innerHTML = [
+            '<ul style="clear:none;margin:0;padding:0;">',
+                '<li></li>',
+            '</ul>'
+        ].join('');
+        list = panel.firstChild;
+        document.body.appendChild(panel);
+    }
 
 })(window.console, true, '#00f');
